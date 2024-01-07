447dc78079da60ffbbb4551f89e0424adaddfd0b
Honza Hodánek <hodanek.honza@gmail.com>
2024-01-07 13:05:55 +0100

    added tabs to food and activity list for better look, used Uu5Elements

--- yummy_fit_maing01-hi/src/core/main-page/modal-button.js
+++ yummy_fit_maing01-hi/src/core/main-page/modal-button.js
@@ -1,5 +1,5 @@
 import { Fragment, createVisualComponent, useState } from "uu5g05";
-import { Button, Modal } from "uu5g05-elements";
+import Uu5Elements, { Button, Modal } from "uu5g05-elements";
 import FoodActivityList from "../../bricks/main-page/food-activity-list";
 import TodaysFoodActivityList from "../../bricks/main-page/todays-food-activity-list";
 const ModalOnButton = createVisualComponent({
@@ -7,6 +7,7 @@ const ModalOnButton = createVisualComponent({
     /*@@viewOn:example*/
 
     const [open, setOpen] = useState();
+    const [foodOrActivityList, setFoodOrActivityList] = useState(false);
 
     return (
       <Fragment>
@@ -14,10 +15,28 @@ const ModalOnButton = createVisualComponent({
           {header}
         </Button>
         <Modal header={header} open={open} onClose={() => setOpen(false)}>
-          <h2>{FoodOrActivity ? "Add Food to your day" : "Add Activity to your day"}</h2>
-          <FoodActivityList create={create} list={content} FoodOrActivity={FoodOrActivity} />
-          <h3>{FoodOrActivity ? "Todays Food" : "Todays Activity"}</h3>
-          <TodaysFoodActivityList deleteData={deleteData} list={todayData} foodOrActivity={FoodOrActivity} />
+          <Uu5Elements.Tabs
+            itemList={[
+              {
+                label: FoodOrActivity ? "Food List" : "Activity List",
+                children: (
+                  <>
+                    <h2>{FoodOrActivity ? "Add Food to your day" : "Add Activity to your day"}</h2>
+                    <FoodActivityList create={create} list={content} FoodOrActivity={FoodOrActivity} />
+                  </>
+                ),
+              },
+              {
+                label: FoodOrActivity ? "Today Food List" : "Today Activity List",
+                children: (
+                  <>
+                    <h3>{FoodOrActivity ? "Todays Food" : "Todays Activity"}</h3>
+                    <TodaysFoodActivityList deleteData={deleteData} list={todayData} foodOrActivity={FoodOrActivity} />
+                  </>
+                ),
+              },
+            ]}
+          />
         </Modal>
       </Fragment>
     );
